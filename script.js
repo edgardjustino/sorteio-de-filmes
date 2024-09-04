b0808c889d06d7aa84eaa44c176ef23828b784d0
edgardjustino <eddiejustino@gmail.com>
2024-09-03 21:50:07 -0300

    fixed: javascrips conditions now work

--- script.js
+++ script.js
@@ -11,33 +11,47 @@ addBtn.addEventListener('click', (e) => {
 
   //show on the screen
   let newOption = document.createElement('li');
-  newOption.innerHTML = `<span class="opcao"><h2 id='opcao'>${optionName}</h2></span>` + `<span onclick="deleteOption(this)"><i class="fa-solid fa-trash"></i></span>`;
+  newOption.innerHTML =
+    `<span class="opcao"><h2 id='opcao'>${optionName}</h2></span>` +
+    `<span onclick="deleteOption(this)"><i class="fa-solid fa-trash"></i></span>`;
 
   ulOptions.appendChild(newOption);
 
   inputText.value = '';
 
   optionsArray.push(optionName);
+  if (optionsArray.length === 1){
+    result.innerText = 'Coloque mais uma opção'
+  }
+  if (optionsArray.length > 1){result.innerText = '';}
 });
-
 
 function deleteOption(x) {
   //console.log(li)
   li = x.parentElement;
-  optionName = li.querySelector('.opcao')
+  optionName = li.querySelector('.opcao');
   //.innerText
-  
+
   //optionsArray -> remover elemento = innerText do li
-  index = optionsArray.indexOf(optionName)
-  optionsArray.splice(index, 1)
+  index = optionsArray.indexOf(optionName);
+  optionsArray.splice(index, 1);
 
   li.remove();
 }
 
 function sortear() {
-  let index = Math.floor(Math.random()*optionsArray.length)
-  console.log(index);
-  result.innerText = optionsArray[index];
+  if (optionsArray.length === 0) {
+    result.innerText = 'Por favor, coloque uma opção';
+    
+  } else if (optionsArray.length === 1) {
+    result.innerText = 'Coloque mais uma opção';
+  } else {
+    let index = Math.floor(Math.random() * optionsArray.length);
+    console.log(index);
+    result.innerText = optionsArray[index];
+  }
+
+ 
 }
 
 // function deleteAll (){
